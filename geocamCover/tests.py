649e1f46c5ffba28186e64a6b143ed0c3bc41dc6
Trey Smith <trey.smith@nasa.gov>
2011-05-20 10:40:22 -0700

    created app template

---
+++ geocamCover/tests.py
@@ -0,0 +1,15 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+from django.test import TestCase
+
+
+class geocamCoverTest(TestCase):
+    """
+    Tests for geocamCoverWeb
+    """
+    def test_geocamCover(self):
+        pass
