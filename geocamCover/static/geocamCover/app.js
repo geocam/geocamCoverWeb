54e20f7b6fc5d232465d8159761c296fe3cda88e
Asem Radhwi <asem.radhwi@sv.cmu.edu>
2011-06-03 18:05:00 -0700

    Changed the javascript to utilize jquery google maps

--- geocamCover/static/geocamCover/app.js
+++ geocamCover/static/geocamCover/app.js
@@ -1,93 +1,46 @@
-var map;
-var clicked_location;
-var items;
-
 $(document).ready(function () {
 
-            //MOFFETT FIELD COORDINATES
-            var latlng = new google.maps.LatLng(37.41288, -122.052934);
-            $('#map_canvas').gmap({
-                        'center': latlng,
-                        'mapTypeId': google.maps.MapTypeId.ROADMAP,
-                        'callback': function () {
-                            $.getJSON('/geocamCover/hello.json', function(data) {
-                                $.each(data.places, function(key, val) {
-                                    latlng = new google.maps.LatLng(val.latitude, val.longitude);
-                                    $('#map_canvas').gmap('addMarker', {'position': latlng, 'title': val.name});
-                                })
-                            });
-
-
-                        }
+    //MOFFETT FIELD COORDINATES
+    var latlng = new google.maps.LatLng(37.41288, -122.052934);
+    $('#map_canvas').gmap({
+                'center': latlng,
+                'mapTypeId': google.maps.MapTypeId.ROADMAP,
+                'zoom': 12,
+                'callback': function () {
+                    $.getJSON('/geocamCover/hello.json', function(data) {
+                        $.each(data.places, function(key, val) {
+                            latlng = new google.maps.LatLng(val.latitude, val.longitude);
+                            $('#map_canvas').gmap('addMarker', {'position': latlng, 'title': val.name});
+                        })
                     });
 
-            //ADDING MARKERS WHEN THE USER CLICKS ON THE MAP
-            $('#map_canvas').gmap({'callback':function(map){
-               $(map).click(function(event){
-                   alert('hi');
-               })
-               }
-            });
-//
-//            var myOptions = {
-//                zoom: 12,
-//                center: latlng,
-//                mapTypeId: google.maps.MapTypeId.ROADMAP
-//            };
-//
-//            map = new google.maps.Map(document.getElementById("map_canvas"),
-//                    myOptions);
-//
-//            google.maps.event.addListener(map, 'click', function(event) {
-//                clicked_location = event.latLng;
-//                $('div#forms').css("visibility", "visible");
-//                $('input#name').focus();
-//
-//                return false;
-//            })
-//
-//            $("#marker_form").submit(function() {
-//                var place = placeMarker(clicked_location, $('input#name').val());
-//
-//
-//                var new_place = JSON.stringify({"latitude": clicked_location.lat(),
-//                            "longitude": clicked_location.lng() ,
-//                            "name": $('input#name').val() });
-//
-//                $.post('/geocamCover/place/', new_place, function(data){
-////                    alert("Data Loaded: "+data);
-//                });
-//
-//                $('input#name').val("");
-//                return place;
-//            }
-//            );
-//
-//            $.getJSON('/geocamCover/hello.json', function(data) {
-//
-//                $.each(data.places, function(key, val) {
-////        alert("latitude=" + val.latitude + ", longitude=" + val.longitude);
-//                    placeMarker(new google.maps.LatLng(val.latitude, val.longitude, true), val.name);
-//
-//                })
-//            });
-        });
 
-
-function placeMarker(loc, name) {
-    var marker = new google.maps.Marker({
-                position: loc,
-                map: map,
-                title: name
+                }
             });
 
-    $('div#forms').css("visibility", "hidden");
+    //ADDING MARKERS WHEN THE USER CLICKS ON THE MAP
+    $('#map_canvas').gmap({'callback':function(map) {
+        $(map).click(function(event) {
+            $('div#forms').css("visibility", "visible");
+            $('input#name').focus();
+            var clicked_position = event.latLng;
 
-    return false;
-}
+            $("#marker_form").submit(function() {
 
-// function addName(name) {
-// 	alert(name);
-// 	marker.title = name;
-// 	alert(marker.title);
-// }
+                $("#map_canvas").gmap('addMarker', {'position':clicked_position, 'title': $('input#name').val()});
+
+                var new_place = JSON.stringify({"latitude": clicked_position.lat(),
+                            "longitude": clicked_position.lng() ,
+                            "name": $('input#name').val() });
+
+                $.post('/geocamCover/place/', new_place, function(data) {
+                });
+
+                $('input#name').val("");
+                $('div#forms').css("visibility", "hidden");
+                return false;
+            });
+        })
+    }});
+
+});
